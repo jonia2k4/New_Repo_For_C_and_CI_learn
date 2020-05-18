ff05635727b7d2fd4c5987477557271f2eb5027a
jonia2k4 <65356507+jonia2k4@users.noreply.github.com>
2020-05-18 16:23:30 -0500

    added prime number function

--- CUnit_code_for_Swapnums.c
+++ CUnit_code_for_Swapnums.c
@@ -2,6 +2,7 @@
 #include <CUnit/Basic.h>
 
 void swap(int*, int*);
+int prime_number(int);
 
 void swap(int *a, int *b)
 {
@@ -11,6 +12,38 @@ void swap(int *a, int *b)
    *a = temp;   
 }
 
+int prime_number(int num)
+{
+/* prime numbers: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 
+               53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, */
+int i;
+int p;
+
+for(i=1; i<=num; i++)
+   {
+      if(num%i==0)
+      {
+         p++;
+      }
+   }
+   if(p==2)
+   {
+     return 1;
+   }
+   else
+   {
+    return 0;
+   }
+}
+
+void Prime_numberCUnitTest(int N)
+{
+
+CU_ASSERT(prime_number(19) == 1);
+CU_ASSERT(prime_number(1) == 0);
+CU_ASSERT(prime_number(67) == 1);
+CU_ASSERT(prime_number(100) == 0);
+}
 
 void Swap_number_CUnitTest()
  {
@@ -28,6 +61,7 @@ CU_initialize_registry();
 CU_pSuite suite = CU_add_suite("swapnum_test", 0, 0);
 
 CU_add_test(suite, "swap_fun", Swap_number_CUnitTest);
+CU_add_test(suite, "prime_fun", Prime_numberCUnitTest);
 
 CU_basic_set_mode(CU_BRM_VERBOSE);
 CU_basic_run_tests();
