7d2249fbe5d175481019db2ceb66fded5e1409c5
jonia2k4 <65356507+jonia2k4@users.noreply.github.com>
2020-05-18 20:10:45 -0500

    added Validate Pwd

--- CUnit_code_for_Swapnums.c
+++ CUnit_code_for_Swapnums.c
@@ -4,7 +4,7 @@
 void swap(int*, int*);
 int prime_number(int);
 int largest_in_Array(int[],int); 
-float findAreaTriangle(float,float,float);
+int Validate_Pwd(char a[]);
 
 void swap(int *a, int *b)
 {
@@ -51,29 +51,65 @@ int largest_in_Array(int arr[], int n)
     return max; 
 }
 
-float findAreaTriangle(float a, float b, float c) 
-{ 
-    // Length of sides must be positive and sum of any two sides 
-    // must be smaller than third side. 
-    if (a < 0 || b < 0 || c <0 || (a+b <= c) || 
-        a+c <=b || b+c <=a) 
-    { 
-        exit(0); 
-    } 
-    float s = (a+b+c)/2; 
-    return (s*(s-a)*(s-b)*(s-c)); 
-} 
 
-void AreaTriangleCUnitTest()
+int Validate_Pwd(char a[])
 {
-  float a = 3.0; 
-  float b = 4.0; 
-  float c = 5.0; 
-  float result;
-  result = findAreaTriangle(a, b, c);
-  CU_ASSERT_EQUAL(result,6.0);
+    char c;
+    int len,i,flag1=0,flag2=0,flag3=0,flag4=0;
+    len=strlen(a);
+    if(len<6)
+    flag1=1;
+    else
+    {
+        for(i=0;i<len;i++)
+        if((a[i]>=48&&a[i]<=58))
+        {
+            flag2=0;
+
+            break;
+        }
+        else
+        flag2=1;
+
+        for(i=0;i<len;i++)
+        if((a[i]>=65&&a[i]<=90))
+        {
+            flag3=0;
+
+            break;
+        }
+        else
+        flag3=1;
+
+        for(i=0;i<len;i++)
+        if(a[i]=='#'||a[i]=='$'||a[i]=='*'||a[i]=='&')
+        {
+            flag4=0;
+
+            break;
+        }
+        else
+        flag4=1;
+
+
+    }
+    if(flag1==1||flag2==1||flag3==1||flag4==1)
+    return 0;
+    else
+    return 1;
 }
 
+
+void ValidtaePwd_CUnitTest()
+{
+ /*("Enter your password that should satisfy the following criteria\n1.
+password should contain atleast one capital letter\n2. 
+should have one digit from(o-9)\n3. atleast one special charecter($,*,#,&)\n4.
+length should be atleast 6\n");*/
+
+CU_ASSERT(Validate_Pwd("J1$abc") == 1);
+	
+}	
 void Prime_numberCUnitTest()
 {
 
@@ -113,7 +149,7 @@ CU_pSuite suite = CU_add_suite("swapnum_test", 0, 0);
 CU_add_test(suite, "swap_fun", Swap_number_CUnitTest);
 CU_add_test(suite, "prime_fun", Prime_numberCUnitTest);
 CU_add_test(suite, "Maxarr_fun",largestnumberin_ArrayCUnitTest);
-CU_add_test(suite, "areaTriangle",AreaTriangleCUnitTest);
+CU_add_test(suite, "ValidatePwd",ValidtaePwd_CUnitTest);
 	
 CU_basic_set_mode(CU_BRM_VERBOSE);
 CU_basic_run_tests();
